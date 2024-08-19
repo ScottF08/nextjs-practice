8a77caaa68a91119e234e870f0e2058ea38cd82a
“sforrest” <sforrest@eab.com>
2024-08-18 19:01:48 -0500

    Chapter 9

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,13 @@
-const Page = () => {
-  return <p>Customers Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import { fetchCardData } from '@/app/lib/data';
+const Page = async () => {
+  const { totalPaidInvoices } = await fetchCardData();
+  return (
+    <>
+      <p>Customers Page</p>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+    </>
+  );
 }
 
 export default Page;
