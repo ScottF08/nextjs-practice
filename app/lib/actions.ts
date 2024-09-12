5fc007009a409bafc92361440bc5794e56acda5b
“sforrest” <sforrest@eab.com>
2024-09-12 08:30:26 -0500

    Chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,10 +29,14 @@ export const createInvoice = async (formData: FormData ) => {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch{
+    return { message: 'Database Error: Failed to Create Invoice.' };
+  }
   revalidate();
 };
 
@@ -43,15 +47,26 @@ export const updateInvoice = async (id: string, formData: FormData) => {
     status: formData.get('status'),
   });
   const amountInCents = amount * 100;
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch {
+    return { message: 'Database Error: Failed to Update Invoice.' }
+  }
   revalidate();
 };
 
 export const deleteInvoice = async (id: string) => {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+
+  } catch {
+    return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
 };
